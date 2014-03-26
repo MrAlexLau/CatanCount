3a93232aa0b5d5e017929d192f6167b837a6e5e8
mralexlau <laux0083@gmail.com>
2014-03-25 19:19:46 -0500

    Refactor get_new_game, remove unused images.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,65 +1,35 @@
 class GamesController < ApplicationController
   load_and_authorize_resource :game
+
   def index
     @games = Game.where("user_id = (?)", session[:user_id]).order("created_at DESC")
 
     if @games.empty?
-      @game = set_up_new_game
-      @game.save
+      @game = Game.get_new_game(session[:user_id])
       redirect_to game_path(@game) and return
     end
   end
 
-  # GET /games/1
-  # GET /games/1.json
   def show
     @game = Game.find(params[:id])
-
     @actual_series, @expected_series = Roll.get_game_stats(@game.id)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @roll }
-    end
   end
 
   def new
-    @game = set_up_new_game
-    @game.save
-
-    redirect_to @game
+    # since all of the data for a new game is auto populated (like name, user, etc.)
+    # just go ahead and create it
+    create
   end
 
-  # POST /games
-  # POST /games.json
   def create
-    @game = set_up_new_game
-    @game.save
-
+    @game = Game.get_new_game(session[:user_id])
     redirect_to @game
   end
-
 
   def undo_last_roll
     @game = Game.find(params[:id])
     last_roll = @game.rolls.order("created_at DESC").first
     last_roll.destroy if last_roll
-    render nothing: true
+    render nothing: true # chart updated automatically in javascript
   end
-
-  private
-
-  def set_up_new_game
-    game = Game.new()
-    game.create_date = Date.today
-    game.user_id = session[:user_id]
-
-    if current_user && !current_user.is_guest?
-      users_games_today = Game.where("user_id = (?) and create_date = (?)", session[:user_id], Date.today).order("created_at DESC")
-      game.game_name = Date.today.to_s + " Game " + (users_games_today.length + 1).to_s
-    end
-
-    return game
-  end
-
 end
